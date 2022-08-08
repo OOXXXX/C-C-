e39705592d7ffe3293b9cf3f41c8622dc72abc3e
OOXXXX <zhupx2003@126.com>
2022-08-09 04:43:15 +0800

    Day1 End

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d, %d\n", i, j);
+            break;
+        }
+    }
     return 0;
 }
